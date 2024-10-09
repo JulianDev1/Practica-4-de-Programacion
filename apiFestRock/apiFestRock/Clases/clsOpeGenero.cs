8647f3de0020c78368c41781fb52f462723810f9
JulianDev1 <jmolinaruiz10@gmail.com>
2024-10-09 14:20:39 -0500

    Reparacion del modelo

---
+++ apiFestRock/apiFestRock/Clases/clsOpeGenero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using apiFestRock.Models;
+
+namespace apiFestRock.Clases
+{
+    public class clsOpeGenero
+    {   
+        //Atributo
+        private bdEventosEntities oEFR = new bdEventosEntities();
+        //Propiedad
+        public tblGenero tblGen { get; set; }
+        //------------------------------------------
+        public List<tblGenero> listarGenero() { 
+        
+            return oEFR.tblGeneroes // tabla del modelo entity
+                .OrderBy(x => x.Nombre).ToList();
+        
+        
+        
+        }
+    }
+}
